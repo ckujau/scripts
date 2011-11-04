7fe7c24d030bace1fd4946fe29a91f7df723575b
Christian Kujau <github@nerdbynature.de>
2011-11-04 06:31:46 +0100

    Signed-off-by: Christian Kujau <lists@nerdbynature.de>

---
+++ count.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+#
+# (c) 2011, lists@nerdbynature.de
+# Make Perl count :-)
+#
+use strict;
+
+my $c=0;
+my $e=$ARGV[0];
+my $l=length($e);
+
+while ($c <= $e) {
+        my $s=sprintf("%0${l}d", $c);
+        printf "$s\n";
+        $c ++;
+}
