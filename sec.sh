513421e7c7cbeb54306c19c2bf28f88552344fc8
Christian Kujau <github@nerdbynature.de>
2011-11-15 07:12:53 +0100

    Signed-off-by: Christian Kujau <lists@nerdbynature.de>
    	new file:   sec.sh

---
+++ sec.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+#
+# (c) 2010 lists@nerdbynature.de
+#
+# Execute a few security checks, keep state across runs.
+#   rkhunter: http://www.rootkit.nl/projects/rootkit_hunter.html
+#      lynis: http://www.rootkit.nl/projects/lynis.html
+# chkrootkit: http://www.chkrootkit.org/
+#
+STATE=/var/run/sec.state
+MAXAGE=7
+ 
+# unset me!
+# DEBUG=echo
+ 
+# BTS# 231267
+if [ -f "$STATE" -a ! -L "$STATE" ]; then
+	:
+else
+	$DEBUG rm -f "$STATE"
+	$DEBUG touch "$STATE"
+fi
+ 
+check() {
+C="$1"
+AGE=`echo \( $(date +%s) - $(stat -c %Y "$STATE"."$C") \) / 604800 | bc`
+diff -u "$STATE"."$C" "$STATE"."$C".$$ > "$STATE"."$C".diff
+ 
+# If something changed, display the differences
+if   [ -s "$STATE"."$C".diff ]; then
+	cat "$STATE"."$C".diff
+	mv "$STATE"."$C".$$ "$STATE"."$C"
+ 
+# If nothing changed, display statefile anyway (every MAXAGE days)
+elif [ "$AGE" -ge $MAXAGE ]; then
+	cat "$STATE"."$C"
+	rm "$STATE"."$C".$$
+ 
+# If nothing changed, clean up
+else
+	rm "$STATE"."$C".$$
+fi
+}
+ 
+ 
+case $1 in chkrootkit)
+	$DEBUG touch "$STATE".chkrootkit
+	cd /opt/chkrootkit/sbin
+	$DEBUG ./chkrootkit > "$STATE".chkrootkit.$$
+	check chkrootkit
+	;;
+ 
+	rkhunter)
+	$DEBUG touch "$STATE".rkhunter
+	$DEBUG /opt/rkhunter/bin/rkhunter --pkgmgr DPKG --nocolors --logfile /var/log/rkhunter.log \
+				--skip-keypress --report-warnings-only --check > "$STATE".rkhunter.$$
+	check rkhunter
+	;;
+ 
+	lynis)
+	$DEBUG touch "$STATE".lynis
+	cd /opt/lynis
+	yes | $DEBUG ./lynis --checkall --no-colors > "$STATE".lynis.$$
+	check lynis
+	;;
+ 
+	show)
+	for c in chkrootkit rkhunter lynis; do
+		echo "======= $c ======="
+		cat "$STATE"."$c"
+	done
+	;;
+ 
+	RESET)
+	for c in chkrootkit rkhunter lynis; do
+		echo > "$STATE"."$c"
+	done
+	;;
+ 
+	*)
+	echo "Usage: `basename $0` [chkrootkit|rkhunter|lynis]"
+	echo "		    [show]"
+	echo "		    [RESET]"
+	exit 1
+	;;
+esac
