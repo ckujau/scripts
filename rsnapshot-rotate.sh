132a1283fb848c8c8320f18f26bff2cc7703dc77
Christian Kujau <github@nerdbynature.de>
2011-11-21 13:15:56 +0100

    modified:   rsnapshot-rotate.sh 	CONF= added
    	Signed-off-by: Christian Kujau <lists@nerdbynature.de>

--- rsnapshot-rotate.sh
+++ rsnapshot-rotate.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 #
 # (c)2011 lists@nerdbynature.de
+#
 # Forcefully rotate rsnapshot backups, intentionally discard older copies
 #
+CONF="/etc/rsnapshot"
+
 if [ ! $# = 1 ]; then
-	echo "Usage: `basename $0` [host]"
+	HOSTS=`ls "$CONF"/rsnapshot-*.conf | sed 's/.*\/rsnapshot-//;s/\.conf//' | xargs echo | sed 's/ /|/g'`
+	echo "Usage: `basename $0` [$HOSTS]"
 	exit 1
 else
-	CONF=/etc/rsnapshot/rsnapshot-"$1".conf
+	CONF="$CONF"/rsnapshot-"$1".conf
 	 DIR=`awk '/^snapshot_root/ {print $2}' $CONF`
 
 	[ -f "$CONF" ] || exit 2
