686d6060d2f2b6f75ef9952ae52a3e38111db09e
Christian Kujau <github@nerdbynature.de>
2012-03-09 01:12:11 +0100

    new file:   port-mgmt.sh 	Wrapper for MacPorts
    	Signed-off-by: Christian Kujau <lists@nerdbynature.de>

---
+++ port-mgmt.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# MacPorts cleanup
+#
+PATH=/bin:/usr/bin:/opt/local/bin
+LAST="$HOME"/.ports.clean
+umask 0022
+
+case $1 in
+	a)
+	port echo active | awk '{print $1 $2}'
+	;;
+
+	i)
+	port echo inactive | awk '{print $1 $2}'
+	;;
+
+	u)
+	port selfupdate
+	port echo outdated 
+	port upgrade -u outdated
+	if [ -f "$LAST" ]; then
+		A=`stat -f %m "$LAST"`
+		B=`date +%s`
+		# Cleanup every 1209600 seconds (14 days)
+		if [ `echo $B - $A | bc` -gt 1209600 ]; then
+			echo "port clean all..."
+			echo nice -n20 port clean -f --all all > /dev/null
+		fi
+	else
+		touch "$LAST"
+	fi
+	;;
+
+	*)
+	echo "Usage: `basename $0` [i|a|u]"
+	echo "i - list inactive"
+	echo "a - list active"
+	echo "u - upgrade"
+	exit 1
+	;;
+esac
