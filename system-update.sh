a8f8503ed951ae7bd75783277797b2ca17877e60
Christian Kujau <github@nerdbynature.de>
2012-10-27 01:44:54 +0200

    Basic upgrade script for some Linux systems and OpenCSW, to be extended 	new file:   system-update.sh
    	Signed-off-by: Christian Kujau <lists@nerdbynature.de>

---
+++ system-update.sh
@@ -0,0 +1,88 @@
+#!/bin/sh -e
+#
+# (c)2012 Christian Kujau <lists@nerdbynature.de>
+#
+# Automatic upgrades for various distributions.
+#
+if [ ! "$1" = "-f" ]; then
+	echo "Usage: `basename $0` [-f]"
+	echo
+	DEBUG=echo
+else
+	umask 0022
+	PATH=/bin:/usr/bin:/sbin:/usr/sbin:/opt/local/bin:/usr/local/sbin:/opt/csw/bin
+	date
+fi
+
+# Linux
+#
+# Note: For Linux systems "lsb_release" could be used, but may
+# not be installed so we try to determine the distributer the
+# old fashion way.
+#
+if [ $(uname -s) = "Linux" ]; then
+	# Debian/Ubuntu
+	if [ -f /etc/debian_version ]; then
+		APT_LISTCHANGES_FRONTEND=none
+		DEBIAN_FRONTEND=noninteractive
+		$DEBUG apt-get -q update
+		$DEBUG apt-get -q -y -V dist-upgrade
+		$DEBUG apt-get -q clean
+		$DEBUG deborphan --guess-all
+	fi
+
+	# Redhat/Fedora/CentOS
+	if [ -f /etc/redhat-release ]; then
+		# Note: --assumeyes is unknown to older Yum versions
+		$DEBUG yum -y update
+		$DEBUG yum clean all
+	fi
+fi
+
+# Darwin/MacOS X
+if [ $(uname -s) = "Darwin" ]; then
+	# MacOS
+	$DEBUG softwareupdate --install --all --verbose
+
+	# MacPorts
+	if [ -n "$(port version)" ]; then
+		$DEBUG port selfupdate
+		$DEBUG port echo outdated 
+		$DEBUG port upgrade -u outdated
+
+		# Whan was the last time we did "port clean"?
+		LAST="$HOME"/.ports.clean
+		if [ -f "$LAST" ]; then
+			A=`stat -f %m "$LAST"`
+			B=`date +%s`
+			# Cleanup every 1209600 seconds (14 days)
+			if [ `echo $B - $A | bc` -gt 1209600 ]; then
+				echo "port clean -f --all all"
+				$DEBUG nice -n 5 port clean -f --all all > /dev/null
+				touch "$LAST"
+			fi
+		else
+			touch "$LAST"
+		fi
+	fi
+fi
+
+# SunOS/Solaris
+if [ $(uname -s) = "SunOS" ]; then
+	# TODO: IPS
+	:
+
+	# OpenCSW
+	if [ -f /opt/csw/etc/pkgutil.conf ]; then
+		$DEBUG pkgutil --catalog  --upgrade --yes
+	fi
+
+	# pca
+	# TODO: non-interactive updates?
+	if [ -f "$HOME"/.pca ]; then
+		$DEBUG mkdir -p /var/tmp/patches
+		$DEBUG cd patches
+		# Download (and keep) all missing recommended and security patches
+		$DEBUG pca --download --install missingrs
+	fi
+fi
