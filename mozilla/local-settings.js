252a8a40f92d6f665904d595712d350153a681f9
Christian Kujau <github@nerdbynature.de>
2013-04-12 10:25:41 +0200

    new file:   mozilla/firefox.cfg new file:   mozilla/local-settings.js Signed-off-by: Christian Kujau <github@nerdbynature.de>

---
+++ mozilla/local-settings.js
@@ -0,0 +1,7 @@
+/*
+ * local-settings.js
+ * http://kb.mozillazine.org/Locking_preferences
+ *
+ */
+pref("general.config.obscure_value", 0);	// Do not obscure the content with ROT-13
+pref("general.config.filename", "firefox.cfg");
