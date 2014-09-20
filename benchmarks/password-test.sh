1d14ed68cb278972f486ee197f1b82df60d526c1
Christian Kujau <lists@nerdbynature.de>
2014-09-20 16:57:54 -0700

    modified:   benchmarks/password-test.sh
    * pwqgen maxed out at 22 characters (or I don't understand its configuration)
    * spelling fix
    
    Signed-off-by: Christian Kujau <lists@nerdbynature.de>

--- benchmarks/password-test.sh
+++ benchmarks/password-test.sh
@@ -9,11 +9,16 @@
 
 # Number of passwords required
 if [ -z "$1" ]; then
-	echo "Usage: $(basename $0) [lenth] [num]"
+	echo "Usage: $(basename $0) [length] [num]"
 	exit 1
 else
 	LEN="$1"
 	NUM="$2"
+
+	# See the comment for r_pwqgen below
+	if [ $LEN -gt 22 ]; then
+		echo "WARNING: \"length\" is greater than 22, results for pwqgen may not be correct!"
+	fi
 fi
 
 stats() {
@@ -35,10 +40,25 @@ r_pwgen() {
 pwgen -s -1 $LEN $NUM
 }
 
+# OK, pwqgen is somewhat special: while all other programs can generate (a
+# certain number of) passwords of a certain length, pwqgen generates
+# passwords of a certain randomness (in bits). With random=85 (the highest
+# setting), we're only guaranteed to get a password of at least 22 characters:
+#
+# $ for a in {1..10000}; do pwqgen random=85; done | awk '{print length}' | sort -n | head -1
+# => 22
+#
+# The absolute maximum length seems to be 35 characters.
 r_pwqgen() {
-i=0; while [ $i -lt $NUM ]; do
-	pwqgen | cut -c-"$LEN" | egrep -o "^.{$LEN}$" && i=$((i+1))
-done
+# The following is good enough for passwords of length 22 characters and below:
+for a in $(seq 1 10000); do
+	pwqgen random=85
+done | cut -c-"$LEN" | egrep -o "^.{$LEN}$"
+
+# The following would be more correct, but takes much longer too:
+# i=0; while [ $i -lt $NUM ]; do
+#	pwqgen random=85 | cut -c-"$LEN" | egrep -o "^.{$LEN}$" && i=$((i+1))
+# done
 }
 
 r_apg() {
