01cfde26d60b379e86fe5f605ffcd69e5f583105
Christian Kujau <lists@nerdbynature.de>
2014-09-20 17:10:16 -0700

    * More fixes to benchmarks/password-test.sh Signed-off-by: Christian Kujau <lists@nerdbynature.de>

--- benchmarks/password-test.sh
+++ benchmarks/password-test.sh
@@ -7,7 +7,7 @@
 # cracklib-runtime, passwdqc, pwgen, apg, gpw, makepasswd, openssl, GNU/parallel
 #
 
-# Number of passwords required
+# Password length and number of passwords required
 if [ -z "$1" ]; then
 	echo "Usage: $(basename $0) [length] [num]"
 	exit 1
@@ -40,22 +40,25 @@ r_pwgen() {
 pwgen -s -1 $LEN $NUM
 }
 
-# OK, pwqgen is somewhat special: while all other programs can generate (a
-# certain number of) passwords of a certain length, pwqgen generates
-# passwords of a certain randomness (in bits). With random=85 (the highest
-# setting), we're only guaranteed to get a password of at least 22 characters:
 #
+# OK, pwqgen is somewhat special: while all other programs can generate (a
+# certain number of) passwords of a certain length, pwqgen generates only one
+# password of certain randomness (in bits). With random=85 (the highest
+# setting), we're only guaranteed to get a password of at least 22 characters:
 # $ for a in {1..10000}; do pwqgen random=85; done | awk '{print length}' | sort -n | head -1
 # => 22
 #
-# The absolute maximum length seems to be 35 characters.
+# The absolute maximum length seems to be 35 characters:
+# $ for a in {1..10000}; do pwqgen random=85; done | awk '{print length}' | sort -n | tail -1
+# => 35
+#
 r_pwqgen() {
 # The following is good enough for passwords of length 22 characters and below:
-for a in $(seq 1 10000); do
+for a in $(seq 1 $NUM); do
 	pwqgen random=85
 done | cut -c-"$LEN" | egrep -o "^.{$LEN}$"
 
-# The following would be more correct, but takes much longer too:
+# The following would be the correct solution, but also takes much longer:
 # i=0; while [ $i -lt $NUM ]; do
 #	pwqgen random=85 | cut -c-"$LEN" | egrep -o "^.{$LEN}$" && i=$((i+1))
 # done
