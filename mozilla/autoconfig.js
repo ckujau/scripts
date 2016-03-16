8eceedbb873134c0fb9bceacc8b8f2ee2e401faa
Christian Kujau <lists@nerdbynature.de>
2016-03-16 11:01:35 -0700

    * Disable spell checking (but don't lock it) * Don't lock layout.word_select.stop_at_punctuation * Add help text to autoconfig.js
    Signed-off-by: Christian Kujau <lists@nerdbynature.de>

--- mozilla/autoconfig.js
+++ mozilla/autoconfig.js
@@ -8,6 +8,11 @@
  * configuration file there, we'll have to create a symlink to that location.
  * $ sudo ln -s /etc/firefox/mozilla.cfg /usr/lib/firefox/mozilla.cfg
  *
+ * pref         - users can make changes, but will be reset on the next start
+ * defaultPref  - users can make changes, will be saved between sessions
+ * lockPref     - no changes can be made, required by some configuration items
+ * clearPref    - can be used to erase configuration items
+ *
  */
 pref("general.config.obscure_value", 0); // Do not obscure the content with ROT-13
 pref("general.config.filename", "mozilla.cfg");
