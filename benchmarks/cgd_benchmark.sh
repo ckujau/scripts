eb1bb9ef08342153811bb45476ca8111f1ef55cc
Christian Kujau <lists@nerdbynature.de>
2019-02-03 15:18:18 -0800

    cgd_benchmark.sh added
    Signed-off-by: Christian Kujau <lists@nerdbynature.de>

---
+++ benchmarks/cgd_benchmark.sh
@@ -0,0 +1,53 @@
+#!/bin/ksh -e
+#
+# (c)2019 Christian Kujau <lists@nerdbynature.de>
+# Benchmark for NetBSD/CGD. We'd need something similar for other BSD
+# variants too.
+#
+CIPHERS="aes-cbc aes-xts 3des-cbc blowfish-cbc"
+CGD=cgd3
+
+# unset me!
+# DEBUG="count=1"				# This will still run the tests, but
+						# will only read one byte.
+
+# Short circuit into report mode
+if [ "$1" = "report" ] && [ -f "$2" ]; then
+	for c in $(awk '/MODE:/ {print $2}' "$2" | uniq); do
+		NUM=$(grep -c "MODE: $c" "$2")
+		printf "$c  \t"
+		awk "/MODE: $c/ {print \$9, \$11, \$2}" "$2" | sed 's/(//' | \
+ 			awk "{time+=\$1} {speed+=\$2} END {print time/${NUM}, \"sec /\", speed/${NUM}/1024/1024, \"MB/s\"}"
+	done
+	exit $?
+fi
+
+# FIXME: Do we want more options on the command line?
+if [ ! -c /dev/r${1} ] || [ -z "$2" ]; then
+	echo "Usage:    $(basename $0) [devicename] [runs]"
+	echo "          $(basename $0) report [out.txt]"
+	echo ""
+	echo "Example:  $(basename $0) wd0b 5 | tee out.txt"
+	echo "          $(basename $0) report out.txt"
+	exit 1
+else
+	DEV="$1"
+	NUM="$2"
+fi
+
+# RAW
+for i in $(seq 1 $NUM); do
+	printf "MODE: raw     I: $i\t"
+	dd if=/dev/r${DEV}  of=/dev/null bs=1k $DEBUG conv=sync 2>&1 | grep bytes
+done
+
+# CGD
+for c in $CIPHERS; do
+	for i in $(seq 1 $NUM); do
+		printf "MODE: $c I: $i\t"
+		cgdconfig -g -k urandomkey -o /etc/cgd/test_${c} $c
+		cgdconfig ${CGD} /dev/${DEV} /etc/cgd/test_${c}
+		dd if=/dev/r${CGD}a of=/dev/null bs=1k $DEBUG conv=sync 2>&1 | grep bytes
+		cgdconfig -u /dev/${CGD}a
+	done
+done
