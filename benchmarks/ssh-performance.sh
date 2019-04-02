8a578f1069fa143edb2411967e3b33232655279d
Christian Kujau <lists@nerdbynature.de>
2019-04-01 21:16:30 -0700

    ssh-performance.sh: formatting fixes, minor cleanups
    Signed-off-by: Christian Kujau <lists@nerdbynature.de>

--- benchmarks/ssh-performance.sh
+++ benchmarks/ssh-performance.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # (c)2014 Christian Kujau <lists@nerdbynature.de>
+#
 # Poor wo/man's SSH benchmark. Inspired by:
 # > OpenSSH default/preferred ciphers, hash, etc for SSH2
 # > http://security.stackexchange.com/questions/25662/openssh-default-preferred-ciphers-hash-etc-for-ssh2
@@ -9,7 +10,6 @@
 # > OpenBSD: cipher-speed.sh
 # > https://anongit.mindrot.org/openssh.git/tree/regress/cipher-speed.sh
 #
-
 _help() {
 	echo "Usage: $(basename $0) run    [user@][host] [size-in-MB] [runs]"
 	echo "       $(basename $0) report [performance.log] [top]"
@@ -84,56 +84,44 @@ for c in $CIPHER; do
 done
 }
 
-_filter() {
-#
-# FIXME: is this still needed?
-#
-# The progress meter messed up our log file and we'll remove the control
-# characters again with this ugly sed string.
-# Or, use cat-v:  ... | cat -v | sed 's/1\^H.*\^H//' 
-#
-#	grep seconds "$1" | sed 's/[0-9]\x08//g;s/\x08//'
-	grep seconds "$1"
-}
-
 #
 # Reports
 #
 _report() {
 echo "### Top-$TOP overall"
-_filter "$FILE" | sort -rnk10 | tail -$TOP
+grep seconds "$FILE" | sort -rnk10 | tail -$TOP
 echo
 
 echo "### Fastest cipher"
-_filter "$FILE" | awk '/seconds/ {print $4, $10, "seconds"}' | sort -rnk2 | tail -$TOP
+awk '/seconds/ {print $4, $10, "seconds"}' "$FILE" | sort -rnk2 | tail -$TOP
 echo
 
 echo "### Fastest MAC"
-_filter "$FILE" | awk '/seconds/ {print $6, $10, "seconds"}' | sort -rnk2 | tail -$TOP
+awk '/seconds/ {print $6, $10, "seconds"}' "$FILE" | sort -rnk2 | tail -$TOP
 echo
 
 echo "### Fastest Kex"
-_filter "$FILE" | awk '/seconds/ {print $8, $10, "seconds"}' | sort -rnk2 | tail -$TOP
+awk '/seconds/ {print $8, $10, "seconds"}' "$FILE" | sort -rnk2 | tail -$TOP
 echo
 
 echo "### Top-$TOP for each cipher"
-for c in $(_filter "$FILE" | awk '/seconds/ {print $4}' | sort -u); do
+for c in $(awk '/seconds/ {print $4}' "$FILE" | sort -u); do
 	echo "### Cipher: $c"
-	_filter "$FILE" | grep "$c" | sort -rnk10 | tail -$TOP
+	fgrep seconds "$FILE" | grep "$c" | sort -rnk10 | tail -$TOP
 	echo
 done
 
 echo "### Top-$TOP for each MAC"
-for m in $(_filter "$FILE" | awk '/seconds/ {print $6}' | sort -u); do
+for m in $(awk '/seconds/ {print $6}' "$FILE" | sort -u); do
 	echo "### MAC: $m"
-	_filter "$FILE" | grep "$m" | sort -rnk10 | tail -$TOP
+	fgrep seconds "$FILE" | grep "$m" | sort -rnk10 | tail -$TOP
 	echo
 done
 
 echo "### Top-$TOP for each Kex"
-for k in $(_filter "$FILE" | awk '/seconds/ {print $8}' | sort -u); do
+for k in $(awk '/seconds/ {print $8}' "$FILE" | sort -u); do
 	echo "### Kex: $k"
-	_filter "$FILE" | grep "$k" | sort -rnk10 | tail -$TOP
+	fgrep seconds "$FILE" | grep "$k" | sort -rnk10 | tail -$TOP
 	echo
 done
 }
